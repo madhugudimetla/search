f097cb076a03a142231c39711c726fe8e58e38b2
madhu <madhubalu9908@gmail.com>
2024-09-18 19:04:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,137 @@
+import DestinationSearch from './components/DestinationSearch'
+
+import './App.css'
+
+const destinationsList = [
+  {
+    id: 1,
+    name: 'Melaka Mosque',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/melaka-mosque-img.png',
+  },
+  {
+    id: 2,
+    name: 'Shrubland',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/shrubland-img.png',
+  },
+  {
+    id: 3,
+    name: 'New York',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/new-york-img.png',
+  },
+  {
+    id: 4,
+    name: 'Escarpment',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/escarpment-img.png',
+  },
+  {
+    id: 5,
+    name: 'Westminster Abbey',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/westminster-abbey-img.png',
+  },
+  {
+    id: 6,
+    name: 'South Downs National Park',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/south-downs-national-park-img.png',
+  },
+  {
+    id: 7,
+    name: 'National Historic Site',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/national-historic-site-img.png',
+  },
+  {
+    id: 8,
+    name: 'Tower Bridge',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/tower-bridge-img.png',
+  },
+  {
+    id: 9,
+    name: 'Arc Here',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/arc-here-img.png',
+  },
+  {
+    id: 10,
+    name: 'Steeple',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/steeple-img.png',
+  },
+  {
+    id: 11,
+    name: 'Glaciokarst',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/glaciokarst-img.png',
+  },
+  {
+    id: 12,
+    name: 'Parco Nazionale delle Cinque Terre',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/parco-nazionale-delle-cinque-terre-img.png',
+  },{
+    id: 13,
+    name: 'Melaka Mosque',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/melaka-mosque-img.png',
+  },
+  {
+    id: 14,
+    name: 'Shrubland',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/shrubland-img.png',
+  },
+  {
+    id: 15,
+    name: 'New York',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/new-york-img.png',
+  },
+  {
+    id: 16,
+    name: 'Escarpment',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/escarpment-img.png',
+  },
+  {
+    id: 17,
+    name: 'Westminster Abbey',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/westminster-abbey-img.png',
+  },
+  {
+    id: 18,
+    name: 'South Downs National Park',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/south-downs-national-park-img.png',
+  },
+  {
+    id: 19,
+    name: 'National Historic Site',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/national-historic-site-img.png',
+  },
+  {
+    id: 20,
+    name: 'Tower Bridge',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/tower-bridge-img.png',
+  },
+  {
+    id: 21,
+    name: 'Arc Here',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/arc-here-img.png',
+  },
+  {
+    id: 22,
+    name: 'Steeple',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/steeple-img.png',
+  },
+  {
+    id: 23,
+    name: 'Glaciokarst',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/glaciokarst-img.png',
+  },
+  {
+    id: 24,
+    name: 'Parco Nazionale delle Cinque Terre',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/parco-nazionale-delle-cinque-terre-img.png',
+  }
+]
+
+const App = () => <DestinationSearch destinationsList={destinationsList} />
+
+export default App
